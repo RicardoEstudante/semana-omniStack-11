651069dfdc4ce2ecb2e38d34bc954b067bf8bf78
ricardo carvalho <ricardo2010carvalho@hotmail.com>
2020-03-30 19:57:54 -0300

    ...

--- backend/tests/integration/ong-spec.js
+++ backend/tests/integration/ong-spec.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../../src/app');
+const connection = require('../../src/database/connection');
 
 describe('ONG', () => {
     it('should be able to create a new ONG', async () => {
