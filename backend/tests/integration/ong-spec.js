dd82dabb4a63460769245fe4b774756ff3603c64
ricardo carvalho <ricardo2010carvalho@hotmail.com>
2020-03-30 20:55:07 -0300

    finish

--- backend/tests/integration/ong-spec.js
+++ backend/tests/integration/ong-spec.js
@@ -3,16 +3,26 @@ const app = require('../../src/app');
 const connection = require('../../src/database/connection');
 
 describe('ONG', () => {
+    beforeEach(async () => {
+        await connection.migrate.rollback();
+        await connection.migrate.latest();
+    });
+
+    afterAll(async () => {
+        await connection.destroy();
+    });
+
     it('should be able to create a new ONG', async () => {
         const response = await request(app)
         .post('/ongs')
         .send({
             name: "teste",
-            email: "teste@55pbx",
+            email: "teste@55pbx.com",
             whatsapp: "1195959595",
             city: "Morato",
             uf: "SP"
         });
-        console.log(response.body );
+        expect(response.body).toHaveProperty('id');
+        expect(response.body.id).toHaveLength(8)
     });
 });
